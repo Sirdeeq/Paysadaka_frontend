19da23c6f20f7b069e6d43dddd287453ffe2680e
AlameenYahya <alameenaminu321@gmail.com>
2025-01-14 00:29:08 +0100

    Update MasjidForm.tsx

--- src/components/dashboard/MasjidForm.tsx
+++ src/components/dashboard/MasjidForm.tsx
@@ -167,7 +167,7 @@ export const MasjidForm: React.FC = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto mt-6 mb-6 bg-emerald-50">
+    <div className="max-w-4xl mx-auto mt-6 mb-6 bg-emerald-50 px-6 py-6">
       <h2 className="text-emerald-600 text-2xl font-bold mb-6">Register New Masjid</h2>
       <div className="mb-6 flex flex-col items-center">
         <div
